6ef29f64bec769fe787becf692804508046dc268
bluerhino <nie_kl@126.com>
2018-05-28 23:04:10 +0800

    nio测试，及letcode两数相加

---
+++ guice/src/main/java/priv/bluerhino/java/playground/leetcode/juc/guice/module/HelloModule.java
@@ -0,0 +1,17 @@
+package priv.bluerhino.java.playground.leetcode.juc.guice.module;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import priv.bluerhino.java.playground.juc.guice.service.SayHello;
+import priv.bluerhino.java.playground.juc.guice.serviceImpl.SayHelloTom;
+import priv.bluerhino.java.playground.leetcode.juc.guice.service.SayHello;
+
+/**
+ * Created by niekunlin @ 18/5/12.
+ */
+public class HelloModule implements Module {
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(SayHello.class).to(SayHelloTom.class);
+    }
+}
---
+++ guice/src/main/java/priv/bluerhino/java/playground/leetcode/juc/guice/serviceImpl/SayHelloTom.java
@@ -0,0 +1,16 @@
+package priv.bluerhino.java.playground.leetcode.juc.guice.serviceImpl;
+
+import priv.bluerhino.java.playground.juc.guice.service.SayHello;
+import priv.bluerhino.java.playground.leetcode.juc.guice.service.SayHello;
+
+/**
+ * Created by niekunlin @ 18/5/12.
+ */
+public class SayHelloTom implements SayHello {
+    private static final String myName = "tom";
+
+    @Override
+    public String hello(String name) {
+        return "Hello " + name + ".I'm " + myName + ".";
+    }
+}
